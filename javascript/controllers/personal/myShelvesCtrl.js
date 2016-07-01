332b5f4ea82aca38eb7e1f4f87bb60cebf24183f
弥灬氧气 <961532025@qq.com>
2016-07-01 12:37:09 +0800

    1
    1

---
+++ javascript/controllers/personal/myShelvesCtrl.js
@@ -0,0 +1,14 @@
+
+angular.module('controllers.myShelvesCtrl',[])
+    .controller('MyShelvesCtrl',[
+        '$scope',
+        '$console',
+        '$config',
+        '$rootScope',
+        '$stateParams',
+        '$state',
+        '$httpService',
+        '$locals',
+        function($scope,$console,$config,$rootScope,$stateParams,$state,$httpService,$locals){
+
+        }])
