77cf980e1e4e59dba1c5396a532465c7f530d67a
zhuliang007 <zl_sam@163.com>
2016-07-01 18:35:32 +0800

    1
    1

--- javascript/controllers/personal/myShelvesCtrl.js
+++ javascript/controllers/personal/myShelvesCtrl.js
@@ -11,4 +11,27 @@ angular.module('controllers.myShelvesCtrl',[])
         '$locals',
         function($scope,$console,$config,$rootScope,$stateParams,$state,$httpService,$locals){
 
+                var numberOfPerPage = 5;
+                var pageNo = 0;
+                $scope.noMoreLoad = false;
+                $scope.items = [];
+
+                
+                $scope.shelvesLoadMore= function () {
+                        var data = {
+                                "cmd": $config.cmds.productPublic,
+                                "parameters":{
+                                        "type":0,
+                                        "numberOfPerPage":numberOfPerPage,
+                                        "pageNo":pageNo,
+                                        "status":1
+                                },
+                                "token":"MmY1Zjk5N2MtZGY1OC00YTE4LWJhZTItZjUxMTI2NjY0YjM2"
+                        }
+                        $httpService.getJsonFromPost($config.getRequestAction(),data)
+                            .then(function(result){
+                                console.log(result)
+                            })
+                }
+
         }])
