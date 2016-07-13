eaddd8ceaa10e0cd94fa66d551c2b2da8557204f
zhuliang007 <zl_sam@163.com>
2016-07-13 12:22:42 +0800

    1
    1

--- javascript/controllers/pay/payCtrl.js
+++ javascript/controllers/pay/payCtrl.js
@@ -15,13 +15,18 @@ angular.module('controllers.payCtrl',[])
         '$ionicActionSheet',
         '$http',
         '$ionicModal',
-        function($scope,$config,$console,$httpService,$state,$stateParams,$locals,$rootScope,$ionicActionSheet,$http,$ionicModal) {
+        '$alert',
+        function($scope,$config,$console,$httpService,$state,$stateParams,$locals,$rootScope,$ionicActionSheet,$http,$ionicModal,$alert) {
 
             $scope.backImg = $scope.mineAlipay;
             //$console.show($stateParams);
             $scope.price = $stateParams.obj.price;
-
+            $scope.choice = '';
             $scope.goPay=function(obj){
+                if($scope.choice==''){
+                    $alert.show('请选择支付方式');
+                    return;
+                }
                 window.location.href = 'http://erpuat.mengbp.com:8094/wine-market-rest/market/order/h5payOrder?orderCode='+$stateParams.obj.orderCode+'&token='+$stateParams.obj.token;
                 //$state.go($config.controllers.payRouters.name,{id:$stateParams.id,obj:$stateParams.obj,routers:$stateParams.routers});
             }
