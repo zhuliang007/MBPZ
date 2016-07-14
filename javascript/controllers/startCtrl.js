c6fe422108dca91d5bc20d71779a5a60ab77b85c
steavengong <842228597@qq.com>
2016-07-14 13:44:28 +0800

    bdMap

--- javascript/controllers/startCtrl.js
+++ javascript/controllers/startCtrl.js
@@ -110,7 +110,17 @@ angular.module('controllers.start',[])
 
             $scope.parseTime = function(time){
                 if(time){
-                    return DateFormat.format.prettyDate(time);
+                    var tempStrs = time.split(" ");
+                    var dateStrs = tempStrs[0].split("-");
+                    var year = parseInt(dateStrs[0], 10);
+                    var month = parseInt(dateStrs[1], 10) - 1;
+                    var day = parseInt(dateStrs[2], 10);
+                    var timeStrs = tempStrs[1].split(":");
+                    var hour = parseInt(timeStrs[0], 10);
+                    var minute = parseInt(timeStrs[1], 10);
+                    var second = parseInt(timeStrs[2], 10);
+                    var date = new Date(year, month, day, hour, minute, second);
+                    return DateFormat.format.prettyDate(date);
                 }
             }
 
