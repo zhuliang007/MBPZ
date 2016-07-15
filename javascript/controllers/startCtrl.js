06b0c73deb6a683082e232fa5f8eeb10c35eec7d
zhuliang007 <zl_sam@163.com>
2016-07-15 10:49:28 +0800

    update login
    update login

--- javascript/controllers/startCtrl.js
+++ javascript/controllers/startCtrl.js
@@ -24,41 +24,51 @@ angular.module('controllers.start',[])
             $scope.thirdType = 4;
             $scope.userPhone = purl().param('loginAccount');
 
+            console.log($locals.getObject($config.user_local_info))
+
+            if($locals.getObject($config.user_local_info)==null){
+                //登录获取token
+                console.log($scope.userPhone)
+                if($scope.userPhone!=null){
+                    $locals.set($config.u_p, $scope.userPhone);
+                    var data = {
+                        "cmd": $config.cmds.login,
+                        "parameters":{
+                            "loginAccount":$scope.userPhone,
+                            "thirdType":$scope.thirdType
+                        }
+                    }
+                    $httpService.getJsonFromPost($config.getRequestAction(),data)
+                        .then(function(result){
+                            console.log(result)
+                            $scope.userInfo = {
+                                loginToken:result.data.loginToken,
+                                loginAccount:result.data.loginAccount,
+                                id:result.data.id,
+                                city:result.data.city,
+                                cityText:result.data.cityText,
+                                introduce:result.data.introduce,
+                                nickName:result.data.nickName,
+                                province:result.data.province,
+                                provinceText:result.data.provinceText,
+                                sex:result.data.sex,
+                                userImg:result.data.userImg,
+                                userLevel:result.data.userLevel
+                            }
+                            $locals.setObject($config.user_local_info,$scope.userInfo);
+                        })
+                }else if($scope.userPhone==null||$scope.userPhone==''||$scope.userPhone==undefined){
+                    $locals.clearObject($config.user_local_info);
+                }
+             }else{
+                $scope.userInfo = $locals.getObject($config.user_local_info);
+            }
+
+
+
             var url = $location.url();
             if(!url){
                 $state.go($config.controllers.tabsHome.name);
-            }
-
-            //登录获取token
-            if($scope.userPhone!=null){
-                $locals.set($config.u_p, $scope.userPhone);
-                var data = {
-                    "cmd": $config.cmds.login,
-                    "parameters":{
-                        "loginAccount":$scope.userPhone,
-                        "thirdType":$scope.thirdType
-                    }
-                }
-                $httpService.getJsonFromPost($config.getRequestAction(),data)
-                    .then(function(result){
-                        $scope.userInfo = {
-                            loginToken:result.data.loginToken,
-                            loginAccount:result.data.loginAccount,
-                            id:result.data.id,
-                            city:result.data.city,
-                            cityText:result.data.cityText,
-                            introduce:result.data.introduce,
-                            nickName:result.data.nickName,
-                            province:result.data.province,
-                            provinceText:result.data.provinceText,
-                            sex:result.data.sex,
-                            userImg:result.data.userImg,
-                            userLevel:result.data.userLevel
-                        }
-                        $locals.setObject($config.user_local_info,$scope.userInfo);
-                    })
-            }else if($scope.userPhone==null||$scope.userPhone==''||$scope.userPhone==undefined){
-                $locals.clearObject($config.user_local_info);
             }
 
             $scope.autoLogin = function(){
